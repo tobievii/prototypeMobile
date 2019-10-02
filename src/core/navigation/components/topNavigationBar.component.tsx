0bdb562d11fdaf707dadfc428d308d04edcaef6e
Vincent Kgadima <vincent.kgadima@iotnxt.com>
2019-10-02 14:08:22 +0200

    topnav styling fix

--- src/core/navigation/components/topNavigationBar.component.tsx
+++ src/core/navigation/components/topNavigationBar.component.tsx
@@ -15,13 +15,13 @@ import { textStyle } from '@src/components/common';
 import { SafeAreaView } from './safeAreaView.component';
 import { MaterialIcons } from '@expo/vector-icons';
 import { SearchBar } from 'react-native-elements';
-var devices = require('../../../containers/menu/devices');
+const devices = require('../../../containers/menu/devices');
 import { ScrollView } from 'react-native-gesture-handler';
-import { theme } from '../../../app.component'
+import { theme } from '../../../app.component';
 export interface ComponentProps {
   backIcon?: BackIconProp;
   onBackPress?: () => void;
-  navigation: any
+  navigation: any;
 }
 
 export type TopNavigationBarProps = TopNavigationProps & ComponentProps;
@@ -32,41 +32,41 @@ type BackButtonElement = React.ReactElement<TopNavigationActionProps>;
 export class TopNavigationBarComponent extends React.Component<TopNavigationBarProps> {
   state: {
     data: null;
-    search: ""
-    searchbar;
-    platform;
-    theme;
-  }
+    search: ''
+    searchbar: boolean;
+    platform: string;
+    theme: any;
+  };
 
   UNSAFE_componentWillMount() {
-    this.setState({ search: "" })
-    this.closeSearchBar()
-    this.setState({ platform: Platform.OS })
+    this.setState({ search: '' });
+    this.closeSearchBar();
+    this.setState({ platform: Platform.OS });
   }
 
   componentDidMount() {
     setInterval(() =>
       this.setState({ theme: theme })
-      , 1000)
+      , 1000);
   }
 
-  updateSearch = search => {
+  updateSearch = (search: any) => {
     this.setState({ search });
-    const newData = devices.devices.filter(item => {
-      const itemData = `${item.id.toLowerCase()}   
+    const newData = devices.devices.filter((item: { id: { toLowerCase: () => void; }; data: any; }) => {
+      const itemData = `${item.id.toLowerCase()}
       ${item.data} `;
       const textData = this.state.search;
       return itemData.indexOf(textData.toLowerCase()) > -1;
     });
-    this.setState({ data: newData })
+    this.setState({ data: newData });
   };
 
   closeSearchBar = () => {
-    this.setState({ searchbar: false })
+    this.setState({ searchbar: false });
   }
 
   openSearchBar = () => {
-    this.setState({ searchbar: true })
+    this.setState({ searchbar: true });
   }
 
   deviceData(item: any) {
@@ -88,43 +88,41 @@ export class TopNavigationBarComponent extends React.Component<TopNavigationBarP
     );
   };
 
-  Control = (control) => {
-    if (control == "left") {
+  Control = (control: string) => {
+    if (control == 'left') {
       return (<View style={{ flexDirection: 'row' }}>
         <TouchableOpacity style={{ marginRight: 20, opacity: 0.7 }} >
-          <MaterialIcons name="add" size={32} color={theme.color} />
+          <MaterialIcons name='add' size={24} color={theme.color} />
         </TouchableOpacity>
-        <TouchableOpacity style={{ opacity: 0.7 }} onPress={() => { this.openSearchBar() }}>
-          <MaterialIcons name="search" size={32} color={theme.color} />
+        <TouchableOpacity style={{ opacity: 0.7 }} onPress={() => { this.openSearchBar(); }}>
+          <MaterialIcons name='search' size={24} color={theme.color} />
         </TouchableOpacity>
-      </View>)
-    }
-
-    else if (control == "right") {
+      </View>);
+    } else if (control == 'right') {
       return (<View style={{ flexDirection: 'row' }}>
         <TouchableHighlight style={{ marginLeft: 5, opacity: 0.7 }}>
-          <MaterialIcons name="person" size={32} color={theme.color} onPress={() => { this.props.navigation.navigate('Settings'); }} />
+          <MaterialIcons name='person' size={24} color={theme.color} onPress={() => { this.props.navigation.navigate('Settings'); }} />
         </TouchableHighlight>
         <TouchableOpacity style={{ marginLeft: 5, opacity: 0.7 }} onPress={() => { this.props.navigation.navigate('Settings'); }} >
-          <MaterialIcons name="settings" size={32} color={theme.color} />
+          <MaterialIcons name='settings' size={24} color={theme.color} />
         </TouchableOpacity>
         <TouchableOpacity style={{ marginLeft: 5, opacity: 0.7 }}>
-          <MaterialIcons name="notifications" size={32} color={theme.color} />
+          <MaterialIcons name='notifications' size={24} color={theme.color} />
         </TouchableOpacity>
-      </View>)
+      </View>);
     }
   }
 
   searchList = () => {
-    var { data } = this.state;
+    const { data } = this.state;
     if (this.state.search.length > 0) {
       return (
-        <FlatList data={data} style={{ height: "100%" }} renderItem={({ item }) =>
+        <FlatList data={data} style={{ height: '100%' }} renderItem={({ item }) =>
           <TouchableHighlight style={{ height: 50, borderColor: '#6c757d', borderBottomWidth: 1, backgroundColor: theme.backgroundColor }} onPress={() => this.deviceData(item)}>
             <View style={{ width: '100%', marginLeft: 10, flexDirection: 'row', marginTop: -2 }} >
-              <Text style={{ width: '70%', color: theme.color, marginLeft: 10, marginTop: 15 }}>{item["id"]}</Text></View></TouchableHighlight>
-        } keyExtractor={item => item["id"]} />
-      )
+              <Text style={{ width: '70%', color: theme.color, marginLeft: 10, marginTop: 15 }}>{item['id']}</Text></View></TouchableHighlight>
+        } keyExtractor={item => item['id']} />
+      );
     }
   }
 
@@ -137,26 +135,25 @@ export class TopNavigationBarComponent extends React.Component<TopNavigationBarP
           alignment='center'
           style={{ backgroundColor: theme.backgroundColor }}
           subtitleStyle={textStyle.caption1}
-          leftControl={this.Control("left")}
-          rightControls={this.Control("right")}
+          leftControl={this.Control('left')}
+          rightControls={this.Control('right')}
         />
-      </SafeAreaView>)
-    }
-    else {
+      </SafeAreaView>);
+    } else {
       return (
         <SafeAreaView style={themedStyle.safeArea}>
           <SearchBar
-            placeholder="Type Here..."
+            placeholder='Type Here...'
             platform={this.state.platform}
             searchIcon={{ name: 'ios-search', color: theme.color, type: 'ionicon' }}
-            cancelIcon={{ name: 'ios-arrow-back', color: "red", type: 'ionicon' }}
-            clearIcon={{ name: "ios-close-circle", color: "red", type: 'ionicon' }}
+            cancelIcon={{ name: 'ios-arrow-back', color: 'red', type: 'ionicon' }}
+            clearIcon={{ name: 'ios-close-circle', color: 'red', type: 'ionicon' }}
             containerStyle={{ backgroundColor: theme.backgroundColor }}
             inputStyle={{ backgroundColor: theme.color, color: theme.backgroundColor }}
             onChangeText={this.updateSearch}
             value={this.state.search}
-            cancelButtonProps={{ color: "red" }}
-            onCancel={() => { this.closeSearchBar() }}
+            cancelButtonProps={{ color: 'red' }}
+            onCancel={() => { this.closeSearchBar(); }}
           />
           <ScrollView>{this.searchList()}</ScrollView>
         </SafeAreaView>
